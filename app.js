3bad42da3e027353ccb9f28d75f6f16231493c6e
ManiacalMaynard <maynard@maniacalindustries.com>
2022-01-20 15:37:04 -0800

    Adds functional calculations to operands and the Calculate button. Fixes All Clear button bug more directly

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
 const outputDisplay = document.getElementById('outputDisplay')
 
-let output = ''
+// Declaring variables for use in display and the math operation
+let x = ''
+let op = ''
+let y = ''
+let xory = 0
 
+// Displays the string values for the math operation
 function display() {
-    outputDisplay.innerText = output;
+    console.log(x, op, y, xory)
+    outputDisplay.innerText = x + ' ' + op + ' ' + y;
 }
 
-function allClear() {
-    output = '';
-    display(output);}
-
+// Enters the number from the onclick into x or y based on the xory toggle
 function entry(int) {
-    output += int;
-    display(output);
+    if (xory == 0) { x += int }
+    if (xory == 1) { y += int }
+    display()
 }
 
+// Sets operand to correct math symbol, updates OP variable, set xory to 1 (Y setting)
+    function operation(input) {
+    op = input;
+    xory = 1;
+    display();
+}
+
+// Kinda forgot I put this here, guess this is next TODO lolz
 function del() {
     console.log("I am deleting something")
 }
 
-function calculate() {
+// Returns all variable values to starting state
+function allClear() {
+    console.log("Burn it all!")
+    x = ''
+    op = ''
+    y = ''
+    xory = 0
+    display()
 }
+
+// Applies the correct calculation according to the operand, retuns the result and assigns it to X for further operations
+function calculate() {
+    console.log("beep boop here's the final answer.")
+    if (op == '+') { x = parseFloat(x) + parseFloat(y) };
+    if (op == '-') { x = parseFloat(x) - parseFloat(y) };
+    if (op == '*') { x = parseFloat(x) * parseFloat(y) };
+    if (op == '/') { x = parseFloat(x) / parseFloat(y) };
+    op = ''
+    y = ''
+    display()
+}
+
+// How was this more difficult than expected? Anywhoo, thanks for reading my spaghetti and have a wonderful day!
